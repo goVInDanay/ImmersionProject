8b2d000d836592fc6585aec603eb0be7c22719fd
Govind N Acharya <govindnacharya@gmail.com>
2025-06-24 09:29:51 +0530

    Added UI Components

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import Previous from "./Previous";
 import Interviews from "./Interviews";
-
+import Button from '@mui/material/Button'
 function Dashboard(){
     return(
         <div>
             This is dashboard
             <Interviews/>
-            <Link to = '/dashboard/setup'><button>Start Interview</button></Link> 
+            <Link to = '/dashboard/setup'><Button>Start Interview</Button></Link> 
             <Previous/>
         </div>
     )
