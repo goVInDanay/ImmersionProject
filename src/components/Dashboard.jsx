33ea9fa645419725ba5a4da3fa8f87ac021c7dfb
Govind N Acharya <govindnacharya@gmail.com>
2025-06-24 08:26:27 +0530

    Added more components

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import Previous from "./Previous";
+import Interviews from "./Interviews";
 
 function Dashboard(){
     return(
         <div>
             This is dashboard
+            <Interviews/>
             <Link to = '/dashboard/setup'><button>Start Interview</button></Link> 
             <Previous/>
         </div>
