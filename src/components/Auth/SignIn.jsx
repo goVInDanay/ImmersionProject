b0094b6329e8c98d0a3af89fd722bfb0eb22332c
Govind N Acharya <govindnacharya@gmail.com>
2025-06-25 11:28:21 +0530

    Added Sign-in UI

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,11 +1,69 @@
 import Layout from "../Layout/Layout"
-
+import { Link } from "react-router-dom";
+import SignUp from "./SignUp";
+import { useState } from "react";
 function SignIn(){
+    const [role, setRole] = useState("student");
     return(
         <Layout>
-        <div>
-            Sign In Page
-        </div>
+            <section className="bg-gray-50 dark:bg-gray-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
+                        Flowbite    
+                    </a>
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Sign in to your account
+                            </h1>
+                            <div>
+                                <label
+                                    htmlFor="role"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Select Role
+                                </label>
+                                <select
+                                    id="role"
+                                    value={role}
+                                    onChange={(e) => setRole(e.target.value)}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                                >
+                                    <option value="student">Student</option>
+                                    <option value="employee">Employee</option>
+                                </select>
+                            </div>
+                            <form className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label htmlFor="id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{role == "student" ? "Your University Roll Number" : "Your Employee Id"}</label>
+                                    <input type="number" name="number" id="roll-number" className="no-arrows custom-input bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={role == "student" ? "Roll number" : "Employee Id"} required=""/>
+                                </div>
+                                <div>
+                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
+                                        <div className="flex items-center h-5">
+                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                                        </div>
+                                        <div className="ml-3 text-sm">
+                                            <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                        </div>
+                                    </div>
+                                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                                </div>
+                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                                {role != "student" ? <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Don’t have an account yet? <Link to='/sign-up' element={<SignUp/>} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                                </p>
+                                :<></>}
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                </section>
         </Layout>
     )
 }
