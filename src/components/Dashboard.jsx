20e26a1df76ebcaa29ffc4fc34b6aff7b60e9631
Govind N Acharya <govindnacharya@gmail.com>
2025-06-26 12:19:13 +0530

    Updated Ui

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 import Previous from "./Previous";
 import Interviews from "./Interviews";
+import Layout from './Layout/Layout'
 import Button from '@mui/material/Button'
 function Dashboard(){
     return(
-        <div>
-            This is dashboard
+        <Layout>
             <Interviews/>
-
             <Previous/>
-        </div>
+        </Layout>
     )
 }
 export default Dashboard
