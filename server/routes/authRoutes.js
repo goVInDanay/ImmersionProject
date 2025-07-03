07040373be95ab3b2ceeab00de50abcda0287ae5
Govind N Acharya <govindnacharya@gmail.com>
2025-07-03 10:25:28 +0530

    Changed Routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup } = require("../controllers/signUpController");
-const { signin } = require("../controllers/signInController");
+const { signup, signin } = require('../controllers/authcontroller');
 
 router.post("/signup", signup); 
 router.post("/signin", signin); 
