1ff1fdee3ea199370166c97a27fe92d0b587dba6
Govind N Acharya <govindnacharya@gmail.com>
2025-07-04 08:40:59 +0530

    Changed Index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,24 @@
-const { Sequelize } = require("sequelize");
+const Sequelize = require("sequelize");
 const path = require("path");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: path.join(__dirname, "..", "database.sqlite")
+  storage: path.join(__dirname, "..", "database.sqlite"),
 });
 
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+
+// Import models
 db.User = require("./user.model")(sequelize, Sequelize);
+db.Interview = require("./interview.model")(sequelize, Sequelize);
+
+// Setup associations
+Object.values(db).forEach((model) => {
+  if (model.associate) {
+    model.associate(db);
+  }
+});
 
 module.exports = db;
