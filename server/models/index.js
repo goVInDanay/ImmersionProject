1db52db4e726e108d98f3702daae6c918bccbf4a
Govind N Acharya <govindnacharya@gmail.com>
2025-07-05 11:57:22 +0530

    Changed DB

--- server/models/index.js
+++ server/models/index.js
@@ -9,12 +9,8 @@ const sequelize = new Sequelize({
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-
-// Import models
 db.User = require("./user.model")(sequelize, Sequelize);
 db.Interview = require("./interview.model")(sequelize, Sequelize);
-
-// Setup associations
 Object.values(db).forEach((model) => {
   if (model.associate) {
     model.associate(db);
